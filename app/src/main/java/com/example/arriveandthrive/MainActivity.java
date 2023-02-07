3f77c1facfa569377cacb21e2d6855c7d2c50f40
Clint-G-BCIT <cgaudet9@my.bcit.ca>
2023-02-07 14:40:01 -0800

    beepboo00p

--- app/src/main/java/com/example/arriveandthrive/MainActivity.java
+++ app/src/main/java/com/example/arriveandthrive/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String boop = "boop";
+        String beepb00p = "beepb00p";
     }
 }
